60be6de5d420e529a6a2abb7470e54912db14d32
mgorbatenkov <mgorgatenkov@yahoo.com>
2021-09-03 15:33:13 +0300

    Added verification

--- src/test/java/m/gorbatenkov/FormFiller.java
+++ src/test/java/m/gorbatenkov/FormFiller.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,12 +21,11 @@ public class FormFiller {
     @Test
     void fillTest() {
         open("https://demoqa.com/automation-practice-form");
-
         $("#firstName").setValue("Mary");
         $("#lastName").setValue("Sue");
         $("#userEmail").setValue("Mary@sue.org");
         $("#gender-radio-2 + label").click();
-        $("#userNumber").setValue("+70000000");
+        $("#userNumber").setValue("7000000000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption("1984");
         $(".react-datepicker__month-select").selectOptionByValue("4");
@@ -33,16 +33,27 @@ public class FormFiller {
         $("#subjectsInput").setValue("Maths").pressEnter();
         $(byText("Reading")).click();
         $("#uploadPicture").uploadFile(new File("src/test/java/resources/Logo.png"));
-        $("#currentAddress").setValue(
-                "30 Aulike St.\n" +
-                "Suite 105\n" +
-                "Kailua, Hawaii 96734\n" +
-                "Phone: (808) 266-1222\n" +
-                "Fax: (808) 266-1226");
-        $("#state div").scrollIntoView(true).click();
+        $("#currentAddress").setValue("Kailua, Hawaii 96734");
+        $("#state").scrollIntoView(true).click();
         $(byText("NCR")).click();
-        $("#city div").click();
+        $("#city").click();
         $(byText("Gurgaon")).click();
+        $("#submit").click();
+
+        $("tbody tr:nth-child(1)").shouldHave(text("Mary Sue"));
+        $("tbody tr:nth-child(2)").shouldHave(text("Mary@sue.org"));
+        $("tbody tr:nth-child(3)").shouldHave(text("Female"));
+        $("tbody tr:nth-child(4)").shouldHave(text("7000000000"));
+        $("tbody tr:nth-child(5)").shouldHave(text("21 May,1984"));
+        $("tbody tr:nth-child(6)").shouldHave(text("Maths"));
+        $("tbody tr:nth-child(7)").shouldHave(text("Reading"));
+        $("tbody tr:nth-child(8)").shouldHave(text("Logo.png"));
+        $("tbody tr:nth-child(9)").shouldHave(text("Kailua, Hawaii 96734"));
+        $("tbody tr:nth-child(10)").shouldHave(text("NCR Gurgaon"));
+
+
+
+
 
     }
 }
